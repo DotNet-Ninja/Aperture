6bea9c7d5b1bc3cbfa717d105f6dd4b7dcf233eb
DotNet Ninja <chaos.monkey@hotmail.com>
2022-09-30 07:37:09 -0500

    Update Roles

--- src/Aperture/Constants/Role.cs
+++ src/Aperture/Constants/Role.cs
@@ -2,5 +2,8 @@
 
 public static class Role
 {
-    public static readonly string Administrator = "PhotoBlogger:Admin";
+    public static readonly string Administrator = "Aperture:Admin";
+    public static readonly string Member = "Aperture:Member";
+    public static readonly string Family = "Aperture:Family";
+    public static readonly string Friend = "Aperture:Friend";
 }
